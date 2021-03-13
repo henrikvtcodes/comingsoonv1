ffc629885ea6674fac2e0521168632b5231dbf83
henrikvtcodes <526473+gb96@users.noreply.github.com>
2021-03-13 18:20:26 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './henriklogo.png';
+// import henrik from './forestpic-removedbg.png'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="spinning henrik logo" />
+      </header>
+      <div>
+        <h1 className="b">
+          COMING SOON
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
